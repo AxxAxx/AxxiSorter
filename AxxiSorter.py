66962610bb495c75068c0ef4bd93acc86f9b4518
Axel Johansson <axeljohansson@duck.com>
2024-04-21 20:56:59 +0200

    update function to handle files taken on the same date/time

--- AxxiSorter.py
+++ AxxiSorter.py
@@ -4,12 +4,23 @@ from datetime import datetime
 from PIL import Image, ExifTags
 import shutil
 from termcolor import colored
+import random
+import string
 os.system('color')
+
+def get_random_string(length):
+    # choose from all lowercase letter
+    letters = string.ascii_lowercase
+    result_str = ''.join(random.choice(letters) for i in range(length))
+    return result_str
 
 number_of_camera_images = 0
 number_of_whatsapp_images = 0
 number_of_movies = 0
+number_of_screenshots = 0
 number_of_fails = 0
+
+created_names = []
 
 folder_path = os.path.join(os.path.abspath(os.getcwd()), "iphone_data")
 
@@ -17,7 +28,7 @@ valid_file_extensions = [".jpg", ".JPG", ".jpeg", ".JPEG", ".png", ".PNG"]
 valid_movie_extensions = [".mov", ".MOV"]
 
 for subfolder_path in os.listdir(folder_path):
-    if(os.path.isdir(subfolder_path)):
+    if(os.path.isdir(os.path.join(folder_path, subfolder_path))):
         for file_name in os.listdir(os.path.join(folder_path, subfolder_path)):
             # Create the old file path
             old_file_path = os.path.join(folder_path,subfolder_path, file_name)
@@ -35,7 +46,6 @@ for subfolder_path in os.listdir(folder_path):
                 
                 # Close the image
                 image.close()
-
 
                 # Check if the metadata exists
                 if metadata is None or file_extension  in valid_movie_extensions:
@@ -72,6 +82,7 @@ for subfolder_path in os.listdir(folder_path):
 
             if file_extension == ".PNG":
                 temp_path = os.path.join(folder_path, "Screenshots")
+                number_of_screenshots += 1
                 if not os.path.exists(temp_path):
                     os.makedirs(temp_path)
 
@@ -88,21 +99,36 @@ for subfolder_path in os.listdir(folder_path):
                         number_of_camera_images += 1
 
 
-            elif file_extension == ".MOV":
+            elif file_extension in valid_movie_extensions:
                 temp_path = os.path.join(folder_path, "Movies")
                 if not os.path.exists(temp_path):
                     os.makedirs(temp_path)
                 number_of_movies += 1
 
-            
+            else:
+                print("#####################################")
+                print("#####################################")
+                print(file_name)
+                print("#####################################")
+                print("#####################################")
 
-            # Create the new folder path
-            new_file_path = os.path.join(temp_path, new_file_name)
+
+
             # Rename the file
             try:
+                if(new_file_name in created_names):
+                    print(new_file_name)
+                    new_file_name = new_file_name.split(".")[0] + "_" + get_random_string(4) + "." + new_file_name.split(".")[1]
+                    
+                created_names.append(new_file_name)
+                print(created_names)
+
+                # Create the new folder path
+                new_file_path = os.path.join(temp_path, new_file_name)
+
                 shutil.copy(old_file_path, new_file_path)
                 #print(str(temp_path.split('\\')[-1]))
-                print(str(file_name) + " copied to folder: " + colored(str(temp_path.split('\\')[-1]), 'green') + " with date: " + colored(str(date_time), 'red'))
+                #print(str(file_name) + " copied to folder: " + colored(str(temp_path.split('\\')[-1]), 'green'))
             except:
                 pass
 
@@ -110,6 +136,7 @@ print()
 print("Copied " + colored(str(number_of_camera_images), 'green') + " camera images")
 print("Copied " + colored(str(number_of_whatsapp_images), 'green') + " Whatsapp images")
 print("Copied " + colored(str(number_of_movies), 'green') + " movies")
+print("Copied " + colored(str(number_of_screenshots), 'green') + " screenshots")
 print("Failed to copy " + colored(str(number_of_fails), 'red') + " files")
 
 input("Press Enter to continue...")
